6e567cf62955db1628e8b4c63cb438418a1c25f6
Neha Kadam <neha@kitchenspurs.com>
2026-04-06 23:11:53 +0530

    fixed basic patterns error

--- practice/moveZeroToEnd.js
+++ practice/moveZeroToEnd.js
@@ -5,12 +5,24 @@ function moveZero(arr) {
     let left = 0
 
     for(let right = 0; right < arr.length; right++) {
-        if(arr[left] === 0 && arr[right] !== 0) {
-            let temp = arr[right]
-            arr[right] = 0
-            arr[left] = temp
-            left++;
+
+
+        // This too is correct but we have a much cleaner solution
+        // if(arr[right] !== 0) {
+        //     let temp = arr[right]
+        //     arr[right] = arr[left]
+        //     arr[left] = temp
+        //     left++
+        // }
+
+        if(arr[right] !== 0) {
+            arr[left] = arr[right]
+            left++
         }
+    }
+
+    for(let i = left; i < arr.length; i++) {
+        arr[i] = 0
     }
 
     return arr
@@ -18,4 +30,14 @@ function moveZero(arr) {
 }
 
 let arr = [0, 1, 0, 3, 12, 0]
+let arr2 = [0, 0, 0, 3, 12, 0]
+let arr3 = [0, 0, 0, 0, 0]
+let arr4 = [1, 0, 1, 0, 0]
+let arr5 = [1, 0, 1, 0, 0, 1]
+let arr6 = [1, 0, 1]
 console.log(moveZero(arr))
+console.log(moveZero(arr2))
+console.log(moveZero(arr3))
+console.log(moveZero(arr4))
+console.log(moveZero(arr5))
+console.log(moveZero(arr6))
--- practice/removeDuplicates.js
+++ practice/removeDuplicates.js
@@ -3,12 +3,13 @@
 // remove duplicates in-place so each unique element appears only once.
 
 function removeDuplicates(arr) {
+    if(arr.length === 0) return [];
     let left = 0
 
     for(let right = 1; right < arr.length; right++) {
         if(arr[left] !== arr[right]) {
-            arr[left + 1] = arr[right]
             left++
+            arr[left] = arr[right]
         }        
     }
 
@@ -19,4 +20,8 @@ function removeDuplicates(arr) {
 }
 
 let arr = [1, 1, 2, 2, 2, 4, 5, 5, 7, 7, 8]
+let arr2 = [1, 1, 2, 2]
+let arr3 = [1, 1]
 console.log(removeDuplicates(arr))
+console.log(removeDuplicates(arr2))
+console.log(removeDuplicates(arr3))
