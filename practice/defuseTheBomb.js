7059be942701fcd6dda6ae2cc6fb1b23fd6b7f58
Neha Kadam <neha@kitchenspurs.com>
2026-04-08 23:00:57 +0530

    fixed issues

--- practice/defuseTheBomb.js
+++ practice/defuseTheBomb.js
@@ -4,38 +4,34 @@
 
 
 function replace(arr, k) {
-    let left = 1;
+    // let left = 0
+    let windowSum = 0
     let result = []
-    let windowSum = 0;
-    let n = arr.length;
-    let right = left + k;
 
-    for(let i = 1; i <= k; i++) {
-        windowSum += arr[i]
+    for(let right = 1; right < k + 1; right++) {
+        windowSum += arr[right]
     }
 
-    result[0] = windowSum;
+    result[0] = windowSum
+    // left++
 
-    for(let i = 1; i < n; i++) {
-
-        if (right >= n) {
-            right = ( i + (n + k)) % n;
-        }
-
-        windowSum += arr[right]
-        windowSum -= arr[left]
-        right++
-        left++
-
-        result[i] = windowSum
+    for(let right = 1; right < arr.length; right++) {
+        
+        let index = (right + k) % arr.length;
+        
+        windowSum += arr[index]
+        windowSum -= arr[right]
+        // left++
+        
+        result[right] = windowSum
     }
 
     return result;
-
-
+   
 }
 
 let arr = [5, 7, 1, 4, 5]
 let arr1 = [5, 7, 1, 4]
 console.log(replace(arr, 3))
 console.log(replace(arr1, 3))
+
--- practice/maxSumArraySum.js
+++ practice/maxSumArraySum.js
@@ -1,28 +1,28 @@
 // Problem: Find the maximum sum of any contiguous subarray of size k.
 
 function maxSum(arr, k) {
-    if (k > arr.length) return null;
-    
-    let left = 0;
-    let windowSum = 0;
-    let maxSum = -Infinity; // for negative numbers
+    if(arr.length < k) return null
 
+    let left = 0
+    let windowSum = 0
+    let maxSum = -Infinity
+    
     for(let right = 0; right < k; right++) {
         windowSum += arr[right]
     }
 
-    maxSum =  Math.max(maxSum, windowSum)
+    maxSum = Math.max(maxSum, windowSum)
 
     for(let right = k; right < arr.length; right++) {
         windowSum += arr[right]
+
         windowSum -= arr[left]
         left++
 
         maxSum = Math.max(maxSum, windowSum)
     }
 
-    return maxSum
-
+    return maxSum === -Infinity ? 0 : maxSum;
 } 
 
 let arr = [2, 7, 5, 1, 3, 2]
